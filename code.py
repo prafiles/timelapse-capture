f42c714ab6ac1ed1670f2df0cb7f442e89d97e4d
Prakhar Shukla <prafiles@yahoo.com>
2021-10-25 12:07:49 +0530

    trying a bit lower exposure

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ while True:
 
     if cur_time.hour < 8 or cur_time.hour > 17:
         print("Capturing Night " + stub)
-        command = '/usr/local/bin/libcamera-still -o low/%s.jpg --shutter 6000000 --denoise cdn_off -q 100 --post-process-file hdr.json' % stub
+        command = '/usr/local/bin/libcamera-still -o low/%s.jpg --shutter 3000000 --denoise cdn_off -q 100 --post-process-file hdr.json' % stub
         stream = os.popen(command)
         output = stream.read()
         time.sleep(5)
