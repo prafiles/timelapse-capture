852e5bbc9ce526700fd2fa60ed838db469212ada
Prakhar Shukla <prafiles@yahoo.com>
2021-11-25 21:14:16 +0530

    Update for path as bullseye default

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ while True:
 
     if cur_time.hour < 8 or cur_time.hour > 17:
         print("Capturing Night " + stub)
-        command = '/usr/local/bin/libcamera-still -o low/%s.jpg --shutter 3000000 --denoise cdn_off -q 100 --post-process-file hdr.json' % stub
+        command = '/usr/bin/libcamera-still -o low/%s.jpg --shutter 3000000 --denoise cdn_off -q 100 --post-process-file hdr.json' % stub
         stream = os.popen(command)
         output = stream.read()
         time.sleep(5)
@@ -16,7 +16,7 @@ while True:
     if cur_time.hour > 5 and cur_time.hour < 20:
         print("Capturing HDR " + stub)
         # Capture HDR shot
-        command = '/usr/local/bin/libcamera-still -o normal/%s.jpg --denoise cdn_off -q 100 --post-process-file hdr.json' % stub
+        command = '/usr/bin/libcamera-still -o normal/%s.jpg --denoise cdn_off -q 100 --post-process-file hdr.json' % stub
         stream = os.popen(command)
         output = stream.read()
         time.sleep(5)
