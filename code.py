14a4bbeb4b28497cfaeca4859a641eadc5396c62
Prakhar Shukla <prafiles@yahoo.com>
2022-01-24 02:44:51 +0530

    update for mistake of deleting stub in clearing everything

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ import datetime, time, sched, os
 
 while True:
     cur_time = datetime.datetime.now()
+    stub = cur_time.strftime("%Y%m%d%H%M")
     print("Capturing: " + stub)
     command = '/usr/bin/libcamera-still -o normal/%s.jpg --denoise cdn_off -q 100 --post-process-file hdr.json --tuning-file imx477_noir.json' % stub
     stream = os.popen(command)
