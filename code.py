420faefe1adfc6fa04c6872cbeb8b60b6dbae2aa
Prakhar Shukla <prafiles@yahoo.com>
2022-01-24 02:51:44 +0530

    update for timer

--- code.py
+++ code.py
@@ -1,13 +1,20 @@
 #!/bin/python3
 from fractions import Fraction
-import datetime, time, sched, os
+import datetime, os, threading
 
-while True:
+def capture():
     cur_time = datetime.datetime.now()
     stub = cur_time.strftime("%Y%m%d%H%M")
     print("Capturing: " + stub)
     command = '/usr/bin/libcamera-still -o normal/%s.jpg --denoise cdn_off -q 100 --post-process-file hdr.json --tuning-file imx477_noir.json' % stub
     stream = os.popen(command)
     output = stream.read()
+    print("Output: " + str(output))
     # Now let's sleep to complete the minute
-    time.sleep(50)
+
+def timer():
+    threading.Timer(60.0, hello_world).start() # called every minute
+    print("Hello, World!")
+
+print("Starting Timer")
+timer()
