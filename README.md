46805e5de221379eb2c5d529e46584dc979f0922
Prakhar Shukla <prakhar@ourea.lan>
2022-04-20 10:31:35 +0000

    new version with fix for loop ending prematurely

--- README.md
+++ README.md
@@ -3,12 +3,5 @@ Pi HQ Cam Timelapse photography
 
 Move day old files to nas.
 ```bash
-0 10 * * * find /home/pi/sources/timelapse-capture/low/* -mtime +1 -exec mv "{}" /home/pi/media/Nemesis/low \;
 0 0 * * * find /home/pi/sources/timelapse-capture/normal/* -mtime +1 -exec mv "{}" /home/pi/media/Nemesis/normal \;
-```
-
-Move all files after the mode has finished
-```bash
-0 9 * * * mv /home/pi/sources/timelapse-capture/low/* /home/pi/media/Nemesis/low
-0 21 * * * mv /home/pi/sources/timelapse-capture/normal/* /home/pi/media/Nemesis/normal
 ```
--- code.py
+++ code.py
@@ -1,9 +1,12 @@
 #!/bin/python3
 from fractions import Fraction
-import datetime, os, threading
+from threading import Timer
+from time import sleep
+from datetime import datetime
+from os import popen
 
 def get_time():
-    cur_time = datetime.datetime.now()
+    cur_time = datetime.now()
     stub = cur_time.strftime("%Y%m%d%H%M")
     return stub
 
@@ -11,14 +14,17 @@ def capture():
     stub = get_time()
     print("Capturing: " + stub)
     command = '/usr/bin/libcamera-still -o normal/%s.jpg --denoise cdn_off -q 100 --post-process-file hdr.json --tuning-file imx477_noir.json' % stub
-    stream = os.popen(command)
+    print("Command: " + command)
+    stream = popen(command)
     output = stream.read()
     print("Output: " + str(output))
-    # Now let's sleep to complete the minute
 
 def timer():
-    threading.Timer(60.0, capture).start() # called every minute
-    print("Capture started: " + get_time())
+    while True:
+        Timer(0, capture).start() # called every minute
+        print("Capture started: " + get_time())
+        # Now let's sleep to complete the minute
+        sleep(60)
 
 print("Starting Timer")
 timer()
